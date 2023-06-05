22e03389d4069aeaea22995a91e03dc446e80fbe
bhavesh <bhaveshchoudhary@9211gmail.com>
2023-06-05 19:17:04 +0530

    fixed

--- routes/user.js
+++ routes/user.js
@@ -17,8 +17,8 @@ router.get(
   })
 );
 
-router.get("/login", passport.authenticate("google"), (req, res, next) => {
-  res.send("Logged in");
+router.get("/login", passport.authenticate("google"), {
+  successRedirect: process.env.FRONTEND_URL,
 });
 
 router.get("/me", isAuthenticated, myProfile);
